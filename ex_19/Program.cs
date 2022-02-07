71ee865ba66a830cbc2361234607df13e1843dbd
fedor <fedmag2010@gmail.com>
2022-02-07 21:54:19 +0300

    ex19.v1

---
+++ ex_19/Program.cs
@@ -0,0 +1,28 @@
+﻿int x = int.Parse(Console.ReadLine());
+int y = int.Parse(Console.ReadLine());
+if (x==0 | y == 0)
+{
+    global::System.Console.WriteLine("X OR Y = 0");
+}
+else if (y > 0)
+{
+    if (x > 0)
+    {
+        global::System.Console.WriteLine("1 четверть");
+    }
+    else
+    {
+        global::System.Console.WriteLine("2 четверть");
+    }
+}
+else
+{
+    if (x > 0)
+    {
+        global::System.Console.WriteLine("4 четверть");
+    }
+    else
+    {
+        global::System.Console.WriteLine("3 четверть");
+    }
+}
