625457ae0424f553d64b9011576c21cefc822a7b
fedor <fedmag2010@gmail.com>
2022-02-07 21:58:20 +0300

    ex26.v1

---
+++ ex_26/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("a = ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("b = ");
+int b = int.Parse(Console.ReadLine());
+for (int count = 0; count < b; count++)
+{
+    a = a + a;
+}
+Console.WriteLine(a);
