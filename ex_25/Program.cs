65374b1241ab14850c05c9ed95d5a9c1580c970c
fedor <fedmag2010@gmail.com>
2022-02-07 21:58:12 +0300

    ex25.v1

---
+++ ex_25/Program.cs
@@ -0,0 +1,11 @@
+﻿global::System.Console.Write("введите число:");
+int a = int.Parse(Console.ReadLine());
+int c = 1;
+int summ = 1;
+for (int count = 1; count<a; c = count)
+{
+    c = c + 1;
+    summ = summ + c;
+    count++;
+}
+Console.WriteLine("сумма от 1 до " + a + " = " + summ);
