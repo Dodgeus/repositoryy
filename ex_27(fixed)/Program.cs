aa6fd81bca2cfc4c82937db1aa15188de4732200
fedor <fedmag2010@gmail.com>
2022-02-07 21:58:52 +0300

    ex27.v1

---
+++ ex_27(fixed)/Program.cs
@@ -0,0 +1,10 @@
+﻿//Определить количество цифр в числе
+Console.Write("введите число:");
+int a = int.Parse(Console.ReadLine());
+int c = 0;
+for (int count = 1; a > 0; count++)
+{
+    a = a / 10;
+    c = count;
+}
+global::System.Console.WriteLine("это "+ c+ " значное число");
