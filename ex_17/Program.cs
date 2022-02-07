c3766b24126c3d84371b80e878f045478333b672
fedor <fedmag2010@gmail.com>
2022-02-07 21:53:59 +0300

    ex17.v1

---
+++ ex_17/Program.cs
@@ -0,0 +1,12 @@
+﻿global::System.Console.WriteLine("введите первое число");
+int a = int.Parse(Console.ReadLine());
+global::System.Console.WriteLine("введите второе число");
+int b = int.Parse(Console.ReadLine());
+if (a*a==b | b*b==a)
+{
+    global::System.Console.WriteLine("одно является квадратом другого");
+}
+else
+{
+    global::System.Console.WriteLine("не является");
+}
