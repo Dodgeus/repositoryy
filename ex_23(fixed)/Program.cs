ff75fbcbbb0af6359d180af9915d5398a446aa84
fedor <fedmag2010@gmail.com>
2022-02-07 21:56:51 +0300

    ex_23.v1

---
+++ ex_23(fixed)/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("введите число:");
+double a = double.Parse(Console.ReadLine());
+double count = 1;
+double c = 1;
+while (count <= a)
+{
+    c = Math.Pow(c, 2);
+    Console.WriteLine(count + " " + c);
+    count++;
+    c = count;
+}
