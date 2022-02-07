1f143ba70cad1451b490ee143b2ae4840a5fb417
fedor <fedmag2010@gmail.com>
2022-02-07 21:58:03 +0300

    ex24.v1

---
+++ ex_24/Program.cs
@@ -0,0 +1,11 @@
+﻿global::System.Console.WriteLine("введтие число:");
+double a = double.Parse(Console.ReadLine());
+int count = 1;
+double c = 1;
+while (count<=a)
+{
+    c = Math.Pow(c, 3);
+    global::System.Console.WriteLine(count + " " + c);
+    count++;
+    c = count;
+}
