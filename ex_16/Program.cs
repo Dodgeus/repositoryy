e3e33f1fff4713aa059ec58ee058f50b91a2225d
fedor <fedmag2010@gmail.com>
2022-02-07 21:53:35 +0300

    ex16.v1

---
+++ ex_16/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("введите номер дня недели:");
+int a = int.Parse(Console.ReadLine());
+if (a>0 && a<8)
+{
+    if (a > 5)
+    {
+        Console.WriteLine("ура сегодня выходной");
+    }
+    else
+    {
+        Console.WriteLine("сегодня будний, иди работай");
+    }
+}
+else
+{
+    Console.WriteLine("такого дня недели не существует");
+}
