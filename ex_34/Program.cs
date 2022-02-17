c121ff6cb42513877ccc6cc39ef620161808b51e
fedor <fedmag2010@gmail.com>
2022-02-17 21:50:20 +0300

    ex_34.v0.1

---
+++ ex_34/Program.cs
@@ -0,0 +1,28 @@
+﻿void ArrayNew(int[] massive)
+{
+    for (int i = 0;i<massive.Length; i++)
+    {
+        massive[i] = new Random().Next(-9,10);
+    }
+}
+void ArrayRevers(int[] col)
+{
+    for (int index = 0; index<col.Length; index++)
+    {
+        col[index] = -col[index];
+    }
+}
+void ArrayPrint(int[] mas)
+{
+    for (int ind = 0; ind<mas.Length; ind++)
+    {
+        Console.Write(mas[ind] + " ");
+    }
+}
+int[] array = new int[8];
+ArrayNew(array);
+ArrayPrint(array);
+Console.WriteLine();
+ArrayRevers(array);
+ArrayPrint(array);
+Console.WriteLine();
